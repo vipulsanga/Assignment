537cfb83aefbe94bbce6668164fbe4571c4f4f56
vipulsanga <127475704+vipulsanga@users.noreply.github.com>
2023-10-09 18:13:14 +0530

    Update ItemServices.java

--- Assignment/src/main/java/com/example/Assignment/service/ItemServices.java
+++ Assignment/src/main/java/com/example/Assignment/service/ItemServices.java
@@ -17,6 +17,16 @@ public class ItemServices
     static
     {
         list.add(new Items(1, "T-Shirt", "Polo t-shirt size is M", 399));
+        list.add(new Items(1, "T-Shirt", "Polo t-shirt size is M", 399));
+        list.add(new Items(2, "Jeans", "Blue denim jeans", 599));
+        list.add(new Items(3, "Hoodie", "Black hoodie size is L", 499));
+        list.add(new Items(4, "Suit", "Black formal suit", 1499));
+        list.add(new Items(5, "Shirt", "Formal shirt size is L", 799));
+        list.add(new Items(6, "Trousers", "Black formal trousers", 699));
+        list.add(new Items(7, "Jacket", "Leather jacket size is XL", 999));
+        list.add(new Items(8, "Sweater", "Gray woolen sweater", 499));
+        list.add(new Items(9, "Polo Shirt", "Blue polo shirt size is M", 349));
+        list.add(new Items(10, "Suit", "Black formal suit", 1499));
     }
     
     // Get all items
